e46f13738fbb0a52d2fbb2997944c2d2a3bbffba
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-02 10:47:56 +0100

    Finished day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,12 +1,110 @@
-﻿using System;
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            List<Move> moves = File.ReadAllLines(Constants.FILE_NAME)
+                .Select(x =>
+                {
+                    string[] parts = x.Split(' ');
+                    _ = Enum.TryParse(CapitalizeFirstLetter(parts[0]), out Direction direction);
+                    return new Move(direction, Int32.Parse(parts[1]));
+                })
+                .ToList();
+
+            Console.WriteLine($"Simulation 1 result: {Simulation1(moves)}");
+            Console.WriteLine($"Simulation 2 result: {Simulation2(moves)}");
         }
+
+        private static int Simulation1(List<Move> moves)
+        {
+            int horizontalPos = 0;
+            int depth = 0;
+
+            foreach (Move move in moves)
+            {
+                switch (move.Direction)
+                {
+                    case Direction.Forward:
+                        horizontalPos += move.Units;
+                        break;
+                    case Direction.Down:
+                        depth += move.Units;
+                        break;
+                    case Direction.Up:
+                        depth -= move.Units;
+                        break;
+                    default:
+                        throw new Exception("Direction not recognized");
+                }
+            }
+
+            return horizontalPos * depth;
+        }
+
+        private static int Simulation2(List<Move> moves)
+        {
+            int horizontalPos = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (Move move in moves)
+            {
+                switch (move.Direction)
+                {
+                    case Direction.Forward:
+                        horizontalPos += move.Units;
+                        depth += aim * move.Units;
+                        break;
+                    case Direction.Down:
+                        aim += move.Units;
+                        break;
+                    case Direction.Up:
+                        aim -= move.Units;
+                        break;
+                    default:
+                        throw new Exception("Direction not recognized");
+                }
+            }
+
+            return horizontalPos * depth;
+        }
+
+        private static string CapitalizeFirstLetter(string input)
+        {
+            return input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+                _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+            };
+        }
+    }
+
+    public readonly struct Move
+    {
+        public Direction Direction { get; init; }
+
+        public int Units { get; init; }
+        
+        public Move(Direction direction, int units)
+        {
+            Direction = direction;
+            Units = units;
+        }
+    }
+
+    public enum Direction
+    {
+        Forward,
+        Down,
+        Up
     }
 }
