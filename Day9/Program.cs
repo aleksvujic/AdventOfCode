35e6febd1e9db0ae9771599ad975237c9d77af11
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-09 09:19:18 +0100

    Added day 9 and upgraded to C# 6

---
+++ Day9/Program.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace Day9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines(Constants.FILE_NAME);
+
+            //Console.WriteLine($"Part 1 result: {Part1(input)}");
+            //Console.WriteLine($"Part 2 result: {Part2(input)}");
+        }
+    }
+}
