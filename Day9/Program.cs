5a11727456b6b6611caf4ac824abdf029ee25194
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-09 09:45:15 +0100

    Finished day 9 part 1

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -6,10 +6,71 @@ namespace Day9
     {
         static void Main()
         {
-            string[] input = File.ReadAllLines(Constants.FILE_NAME);
+            // parse input into grid
+            Grid grid = new Grid(File.ReadAllLines(Constants.FILE_NAME));
 
-            //Console.WriteLine($"Part 1 result: {Part1(input)}");
-            //Console.WriteLine($"Part 2 result: {Part2(input)}");
+            grid.MarkLowestPoints();
+
+            Console.WriteLine($"Part 1 result: {grid.GetRiskLevels()}");
         }
+    }
+
+    class Grid
+    {
+        public Point[][] Mesh { get; }
+
+        public Grid(string[] lines)
+        {
+            Mesh = lines.Select(x => x
+                .ToCharArray()
+                .Select(y => new Point()
+                {
+                    Value = y - '0',
+                    IsLowestPoint = false
+                })
+                .ToArray())
+            .ToArray();
+        }
+
+        public void MarkLowestPoints()
+        {
+            for (int i = 0; i < Mesh.Length; i++)
+            {
+                for (int j = 0; j < Mesh[i].Length; j++)
+                {
+                    bool smallerThanTop = i == 0 || Mesh[i][j].Value < Mesh[i-1][j].Value;
+                    bool smallerThanRight = j == Mesh[i].Length - 1 || Mesh[i][j].Value < Mesh[i][j + 1].Value;
+                    bool smallerThanBottom = i == Mesh.Length - 1 || Mesh[i][j].Value < Mesh[i + 1][j].Value;
+                    bool smallerThanLeft = j == 0 || Mesh[i][j].Value < Mesh[i][j - 1].Value;
+
+                    if (smallerThanTop && smallerThanRight && smallerThanBottom && smallerThanLeft)
+                    {
+                        Mesh[i][j].IsLowestPoint = true;
+                    }
+                }
+            }
+        }
+
+        public int GetRiskLevels()
+        {
+            int riskLevel = 0;
+            for (int i = 0; i < Mesh.Length; i++)
+            {
+                for (int j = 0; j < Mesh[i].Length; j++)
+                {
+                    if (Mesh[i][j].IsLowestPoint)
+                    {
+                        riskLevel += 1 + Mesh[i][j].Value;
+                    }
+                }
+            }
+            return riskLevel;
+        }
+    }
+
+    class Point
+    {
+        public int Value { get; set; }
+        public bool IsLowestPoint { get; set; }
     }
 }
