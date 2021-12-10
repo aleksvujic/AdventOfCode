4ad2d3265fa7fb0a8c7963396098f5f83cafa03e
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-10 09:43:28 +0100

    Finished day 10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -20,6 +20,14 @@ namespace Day10
             { '>', 25137 }
         };
 
+        static readonly Dictionary<char, int> _closingCharacterReward = new()
+        {
+            { ')', 1 },
+            { ']', 2 },
+            { '}', 3 },
+            { '>', 4 }
+        };
+
         static void Main()
         {
             char[][] input = File.ReadAllLines(Constants.FILE_NAME)
@@ -27,6 +35,7 @@ namespace Day10
                 .ToArray();
 
             Console.WriteLine($"Part 1 result: {FindCorruptedLines(input)}");
+            Console.WriteLine($"Part 2 result: {CloseIncompleteLines(input)}");
         }
 
         static int FindCorruptedLines(char[][] input)
@@ -63,6 +72,59 @@ namespace Day10
             }
             
             return errorScore;
+        }
+
+        static long CloseIncompleteLines(char[][] input)
+        {
+            List<long> scores = new();
+
+            foreach (char[] line in input)
+            {
+                // stack data structure is based on LIFO
+                Stack<char> stack = new();
+                bool corruptedLine = false;
+
+                foreach (char symbol in line)
+                {
+                    if (openingSymbols.Contains(symbol))
+                    {
+                        // opening symbol - add it to stack
+                        stack.Push(symbol);
+                    }
+                    else if (closingSymbols.Contains(symbol))
+                    {
+                        if (_openingAndClosingPairs[stack.Peek()] == symbol)
+                        {
+                            // closing symbol matches with top of the stack, chunk correctly closed
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            // closing symbol doesn't match with top of the stack, report error
+                            corruptedLine = true;
+                            break;
+                        }
+                    }
+                }
+
+                // ignore corrupted lines
+                if (corruptedLine)
+                {
+                    continue;
+                }
+
+                // try to fix the line
+                long score = 0;
+                while (stack.Count > 0)
+                {
+                    char symbol = stack.Pop();
+                    score = score * 5 + _closingCharacterReward[_openingAndClosingPairs[symbol]];
+                }
+                scores.Add(score);
+            }
+
+            // return middle value
+            return scores.OrderBy(x => x).ToList()[scores.Count / 2];
         }
     }
 }
