db6bb985729cbd08d9af239a27518af40ff1767c
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-11 09:35:52 +0100

    Added template files for day 11

---
+++ Day11/Program.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+
+namespace Day11
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            char[][] input = File.ReadAllLines(Constants.FILE_NAME)
+                .Select(x => x.ToCharArray())
+                .ToArray();
+
+            Console.WriteLine($"Part 1 result: {FindCorruptedLines(input)}");
+            Console.WriteLine($"Part 2 result: {CloseIncompleteLines(input)}");
+        }
+    }
+}
