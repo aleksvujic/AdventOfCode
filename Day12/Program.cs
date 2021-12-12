c0afe98e38c8bf2b91c0284b47acbd7a95a3acb9
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-12 09:35:43 +0100

    Added template for day 12

---
+++ Day12/Program.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Day12
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] lines = File.ReadAllLines(Constants.FILE_NAME);
+        }        
+    }
+}
