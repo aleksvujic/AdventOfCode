876e8399c646fc8cbf50fdb10169f02fa8166949
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-13 20:19:22 +0100

    Added template for day 14

---
+++ Day14/Program.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+
+namespace Day14
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] lines = File.ReadAllLines(Constants.FILE_NAME);
+            Console.WriteLine("Hello world");
+        }
+    }
+}
