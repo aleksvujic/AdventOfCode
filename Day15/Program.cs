1b62d3a52698482f5a8aec8b4c00fbcc6c5dfc85
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-15 11:57:04 +0100

    Added template for day 15

---
+++ Day15/Program.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace Day15
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] lines = File.ReadAllLines(Constants.FILE_NAME);
+
+            //Console.WriteLine($"Part 1 result: {Solve(pattern, insertions, 10)}");
+            //Console.WriteLine($"Part 2 result: {Solve(pattern, insertions, 40)}");
+        }
+   }
+}
