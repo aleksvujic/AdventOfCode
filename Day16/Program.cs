ded71cb92300c864d381a5d74fc883a49701074c
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-15 13:14:30 +0100

    Template for day 16

---
+++ Day16/Program.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace Day16
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] lines = File.ReadAllLines(Constants.FILE_NAME);
+
+            //Console.WriteLine($"Part 1 result: {GetLowestTotalRisk(GetMap(lines))}");
+            //Console.WriteLine($"Part 2 result: {GetLowestTotalRisk(GetScaledMap(GetMap(lines)))}");
+        }
+    }
+}
