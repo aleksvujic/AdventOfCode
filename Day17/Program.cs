dbb39951a688cda2996c0b0c548770b1d00a4deb
Aleks Vujic <aleks.vujic@gmail.com>
2021-12-16 18:40:43 +0100

    Template for day 17

---
+++ Day17/Program.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace Day17
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines(Constants.FILE_NAME);
+
+            //Console.WriteLine($"Part 1 result: {GetTotalVersion(GetPacket(new Reader(binaryString)))}");
+            //Console.WriteLine($"Part 2 result: {Evaluate(GetPacket(new Reader(binaryString)))}");
+        }
+    }
+}
