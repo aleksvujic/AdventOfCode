f766b70a233201037f52738f5c8ef127a37637f9
Aleks Vujic <aleks.vujic@gmail.com>
2022-12-03 12:50:54 +0100

    Finished day 3

---
+++ 2022/Day3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+
+var lines = File.ReadAllLines(Constants.FILE_NAME).ToList();
+Part1(lines);
+Part2(lines);
+
+void Part1(List<string> lines)
+{
+    var sum = lines
+        .Select(x => x.Chunk(x.Length / 2))
+        .Select(GetItemPriority)
+        .Sum();
+
+    Console.WriteLine($"Part 1 result: {sum}");
+}
+
+void Part2(List<string> lines)
+{
+    var sum = lines
+        .Chunk(3)
+        .Select(GetItemPriority)
+        .Sum();
+    
+    Console.WriteLine($"Part 2 result: {sum}");
+}
+
+int GetItemPriority(IEnumerable<IEnumerable<char>> items)
+{
+    var intersection = items
+        .Skip(1)
+        .Aggregate(
+            new HashSet<char>(items.First()),
+            (h, e) => { h.IntersectWith(e); return h; }
+        )
+        .First();
+
+    return intersection < 'a' ? intersection - 'A' + 27 : intersection - 'a' + 1;
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Solutions of [Advent of Code](https://adventofcode.com/) in C#
 |:---:|:----:|:----:|
 |  1  |  ⭐⭐  |  ⭐⭐  |
 |  2  |  ⭐⭐  |  ⭐⭐  |
-|  3  |  ⭐⭐  |      |
+|  3  |  ⭐⭐  |  ⭐⭐  |
 |  4  |  ⭐⭐  |      |
 |  5  |  ⭐⭐  |      |
 |  6  |  ⭐⭐  |      |
