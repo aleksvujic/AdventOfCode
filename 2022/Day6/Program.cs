dcaa0949505bc2a2aab21b4d7ec159f28230cee6
Aleks Vujic <aleks.vujic@gmail.com>
2022-12-06 16:44:51 +0100

    Finished day 6

---
+++ 2022/Day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+
+var input = File.ReadAllText(Constants.FILE_NAME);
+
+FindMarker(input, 4);
+FindMarker(input, 14);
+
+void FindMarker(string input, int windowLen)
+{
+    for (int i = 0; i < input.Length - windowLen + 1; i++)
+    {
+        var str = input[i..(i + windowLen)];
+        if (ContainsAllUniqueCharacters(str))
+        {
+            Console.WriteLine($"Solution: {i + windowLen}");
+            return;
+        }
+    }
+}
+
+bool ContainsAllUniqueCharacters(string input)
+{
+    return input
+        .GroupBy(x => x)
+        .Select(x => x.Count())
+        .All(x => x == 1);
+}
