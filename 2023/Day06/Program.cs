521c7218b707503756d7bb26c41cc64b3fb99b4a
Aleks Vujic <aleks.vujic@gmail.com>
2023-12-06 19:11:34 +0100

    Finished day 6

---
+++ 2023/Day06/Program.cs
@@ -0,0 +1,49 @@
+﻿using Common;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines(Constants.FILE_NAME)
+    .Select(x => Regex.Replace(x, @"\w+:", string.Empty))
+    .Select(x => x.Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+var races = lines.First()
+    .Zip(lines.Last(), (first, second) => new Race(first, second))
+    .ToArray();
+
+Console.WriteLine($"Part 1 result: {Part1(races)}");
+Console.WriteLine($"Part 2 result: {Part2(races)}");
+
+long Part1(Race[] races)
+{
+    return races.Aggregate(1L, (res, race) => res * GetNumWins(race));
+}
+
+long Part2(Race[] races)
+{
+    long Combine(Func<Race, long> selectorFunc)
+    {
+        return long.Parse(races.Aggregate(string.Empty, (res, val) => res += selectorFunc(val).ToString()));
+    }
+
+    var race = new Race(Combine(x => x.Time), Combine(x => x.Distance));
+
+    return GetNumWins(race);
+}
+
+long GetNumWins(Race race)
+{
+    var winCounter = 0;
+
+    for (int holdDuration = 0; holdDuration <= race.Time; holdDuration++)
+    {
+        var totalDistance = holdDuration * (race.Time - holdDuration);
+
+        if (totalDistance > race.Distance)
+        {
+            winCounter++;
+        }
+    }
+
+    return winCounter;
+}
+
+record Race(long Time, long Distance);
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Solutions of [Advent of Code](https://adventofcode.com/) in C#
 |  3  |  ⭐⭐  |  ⭐⭐  |  ⭐⭐  |
 |  4  |  ⭐⭐  |  ⭐⭐  |  ⭐⭐  |
 |  5  |  ⭐⭐  |  ⭐⭐  |
-|  6  |  ⭐⭐  |  ⭐⭐  |
+|  6  |  ⭐⭐  |  ⭐⭐  |  ⭐⭐  |
 |  7  |  ⭐⭐  |  ⭐⭐  |
 |  8  |  ⭐⭐  |  ⭐⭐  |
 |  9  |  ⭐⭐  |  ⭐⭐  |
