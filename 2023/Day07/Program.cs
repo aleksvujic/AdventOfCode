57c9b8aa44b8fcf539853c01362a09a5245a5a66
Aleks Vujic <aleks.vujic@gmail.com>
2023-12-07 23:41:02 +0100

    Finished day 7

--- 2023/Day07/Program.cs
+++ 2023/Day07/Program.cs
@@ -1,13 +1,21 @@
 ﻿using Common;
 
-var hands = File.ReadAllLines(Constants.FILE_NAME)
-    .Select(x => x.Split())
-    .Select(x => Hand.Parse(x.First(), x.Last()))
+var input = File.ReadAllLines(Constants.FILE_NAME)
+    .Select(x => x.Split());
+
+var hands1 = input
+    .Select(x => Hand.Parse(x.First(), x.Last(), true))
     .ToArray();
 
-Console.WriteLine($"Part 1 result: {Part1(hands)}");
+Console.WriteLine($"Part 1 result: {GetResult(hands1)}");
 
-long Part1(Hand[] hands)
+var hands2 = input
+    .Select(x => Hand.Parse(x.First(), x.Last(), false))
+    .ToArray();
+
+Console.WriteLine($"Part 2 result: {GetResult(hands2)}");
+
+long GetResult(Hand[] hands)
 {
     return hands
         .GroupBy(x => x.HandType)
@@ -17,32 +25,38 @@ long Part1(Hand[] hands)
         .Sum();
 }
 
-record Hand(int[] Cards, int Bid)
+record Hand(int[] Cards, int Bid, bool IsPart1)
 {
-    private static readonly Dictionary<char, int> _cardStrength = new()
-    {
-        { 'A', 14 },
-        { 'K', 13 },
-        { 'Q', 12 },
-        { 'J', 11 },
-        { 'T', 10 },
-        { '9', 9 },
-        { '8', 8 },
-        { '7', 7 },
-        { '6', 6 },
-        { '5', 5 },
-        { '4', 4 },
-        { '3', 3 },
-        { '2', 2 },
-    };
+    private static int _part1JokerVal = 11;
+    private static int _part2JokerVal = 1;
     
-    public static Hand Parse(string cards, string bid)
+    private static Dictionary<char, int> GetCardStrengths(bool isPart1)
+    {
+        return new Dictionary<char, int>()
+        {
+            { 'A', 14 },
+            { 'K', 13 },
+            { 'Q', 12 },
+            { 'J', isPart1 ? _part1JokerVal : _part2JokerVal },
+            { 'T', 10 },
+            { '9', 9 },
+            { '8', 8 },
+            { '7', 7 },
+            { '6', 6 },
+            { '5', 5 },
+            { '4', 4 },
+            { '3', 3 },
+            { '2', 2 },
+        };
+    }
+    
+    public static Hand Parse(string cards, string bid, bool isPart1)
     {
         var cardsArray = cards.ToCharArray()
-            .Select(x => _cardStrength[x])
+            .Select(x => GetCardStrengths(isPart1)[x])
             .ToArray();
         
-        return new Hand(cardsArray, int.Parse(bid));
+        return new Hand(cardsArray, int.Parse(bid), isPart1);
     }
 
     public int HandType
@@ -54,33 +68,48 @@ record Hand(int[] Cards, int Bid)
                 return groups.Count == frequencies.Length && groups.Values.All(frequencies.Contains);
             }
 
-            var groups = Cards
-                .GroupBy(x => x)
-                .ToDictionary(x => x.Key, x => x.Count());
+            var bestHandType = int.MinValue;
+            for (int jokerVal = (IsPart1 ? _part1JokerVal : GetCardStrengths(IsPart1).Values.Min()); jokerVal <= (IsPart1 ? _part1JokerVal : GetCardStrengths(IsPart1).Values.Max()); jokerVal++)
+            {
+                // replace joker with new value
+                var groups = Cards
+                    .Select(x => x == (IsPart1 ? _part1JokerVal : _part2JokerVal) ? jokerVal : x)
+                    .GroupBy(x => x)
+                    .ToDictionary(x => x.Key, x => x.Count());
 
-            // Five of a kind, where all five cards have the same label: AAAAA
-            if (CheckOccurences(groups, new int[] { 5 }))
-                return 6;
-            // Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-            else if (CheckOccurences(groups, new int[] { 1, 4 }))
-                return 5;
-            // Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-            else if (CheckOccurences(groups, new int[] { 2, 3 }))
-                return 4;
-            // Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-            else if (CheckOccurences(groups, new int[] { 1, 1, 3 }))
-                return 3;
-            // Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-            else if (CheckOccurences(groups, new int[] { 1, 2, 2 }))
-                return 2;
-            // One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-            else if (CheckOccurences(groups, new int[] { 1, 1, 1, 2 }))
-                return 1;
-            // High card, where all cards' labels are distinct: 23456
-            else if (CheckOccurences(groups, new int[] { 1, 1, 1, 1, 1 }))
-                return 0;
-            else
-                return -1;
+                int res;
+
+                // Five of a kind, where all five cards have the same label: AAAAA
+                if (CheckOccurences(groups, new int[] { 5 }))
+                    res = 6;
+                // Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+                else if (CheckOccurences(groups, new int[] { 1, 4 }))
+                    res = 5;
+                // Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+                else if (CheckOccurences(groups, new int[] { 2, 3 }))
+                    res = 4;
+                // Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+                else if (CheckOccurences(groups, new int[] { 1, 1, 3 }))
+                    res = 3;
+                // Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+                else if (CheckOccurences(groups, new int[] { 1, 2, 2 }))
+                    res = 2;
+                // One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+                else if (CheckOccurences(groups, new int[] { 1, 1, 1, 2 }))
+                    res = 1;
+                // High card, where all cards' labels are distinct: 23456
+                else if (CheckOccurences(groups, new int[] { 1, 1, 1, 1, 1 }))
+                    res = 0;
+                else
+                    res = -1;
+
+                if (res > bestHandType)
+                {
+                    bestHandType = res;
+                }
+            }
+
+            return bestHandType;
         }
     }
 }
