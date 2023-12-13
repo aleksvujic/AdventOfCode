07d0b7b37af1b0b1a4e512d9b129e4a3e63f11c4
Aleks Vujic <aleks.vujic@gmail.com>
2023-12-13 22:39:04 +0100

    Input parsing day 13

---
+++ 2023/Day13/Program.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+
+Console.WriteLine($"Part 1 result: {Part1()}");
+
+long Part1()
+{
+    var patterns = ParseInput();
+    return -1;
+}
+
+char[][][] ParseInput()
+{
+    var lines = File.ReadAllLines(Constants.FILE_NAME);
+
+    List<char[][]> patterns = [];
+    List<char[]> pattern = [];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var line = lines[i];
+
+        if (!string.IsNullOrEmpty(line))
+        {
+            pattern.Add(line.ToCharArray());
+        }
+
+        var flush = string.IsNullOrEmpty(line) || i == lines.Length - 1;
+        if (flush)
+        {
+            patterns.Add(pattern.ToArray());
+            pattern = [];
+        }
+    }
+
+    return patterns.ToArray();
+}
