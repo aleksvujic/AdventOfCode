c36c249749d6ae427853c626a37bb646b87e47d7
Aleks Vujic <aleks.vujic@gmail.com>
2023-12-19 19:16:35 +0100

    Finished day 15 part 1

---
+++ 2023/Day15/Program.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+
+Console.WriteLine($"Part 1 result: {Part1()}");
+
+long Part1()
+{
+    var sequences = ParseInput();
+    return sequences
+        .Select(GetHash)
+        .Sum();
+}
+
+string[] ParseInput()
+{
+    return File.ReadAllText(Constants.FILE_NAME)
+        .Split(',', StringSplitOptions.RemoveEmptyEntries);
+}
+
+long GetHash(string input)
+{
+    long hash = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        hash += input[i];
+        hash *= 17;
+        hash %= 256;
+    }
+
+    return hash;
+}
